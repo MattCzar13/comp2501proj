28b08b5bbcae309b820df9c4b891c0cdc1d47988
Pierson Michalski <PIERSONMICHALSKI@cmail.carleton.ca>
2022-04-12 16:26:25 -0400

    shield functionalty is now working

--- player_game_object.h
+++ player_game_object.h
@@ -4,16 +4,25 @@
 #include "game_object.h"
 #include <string>
 
+#include <glm/glm.hpp>
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include "shader.h"
+#include <vector>
+
 namespace game {
 
     // Inherits from GameObject
     class PlayerGameObject : public GameObject {
 
         public:
-            PlayerGameObject(const glm::vec3 &position, GLuint texture, GLint num_elements, std::string tag);
+            PlayerGameObject(const glm::vec3 &position, GLuint texture, GLint num_elements, std::string tag, GLuint shield);
 
             // Update function for moving the player object around
             void Update(double delta_time) override;
+            void Render(Shader& shader) override;
 
             void addHealth(int h);
             void addShieldTimer(int t);
@@ -24,6 +33,7 @@ namespace game {
            
             int health_;
             float shield_timer_;
+            GLuint shield_;
 
 
     }; // class PlayerGameObject
